948ca03d57843104bc07f956fba9f6b941166e56
jellymando <cicikdi@naver.com>
2021-07-04 15:02:49 +0900

    feat: Router 추가 - /placeList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import PlaceList from "./pages/PlaceList";
 
 function App() {
-  return <PlaceList />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/placeList">
+          <PlaceList />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../components/Map";
+
+const Home = () => {
+  return (
+    <>
+      <Map />
+    </>
+  );
+};
+
+export default Home;
