f34ff1dc9569a15b5f8cda929dd94debda7ead59
jellymando <cicikdi@naver.com>
2021-07-04 13:02:11 +0900

    feat: styled-components module 추가

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,25 +1,20 @@
-import React, { useRef, useEffect } from 'react';
+/* eslint-disable no-undef */
+import React, { useRef, useEffect } from "react";
+import { MapContainer } from "./styled";
 
 const Map = () => {
-    const mapRef = useRef(null);
+  const mapRef = useRef(null);
 
-    useEffect(() => {
-        if (!mapRef.current) return;
-        const options = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
-        };
-        const map = new kakao.maps.Map(mapRef.current, options);
-    }, [mapRef]);
+  useEffect(() => {
+    if (!mapRef.current) return;
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(mapRef.current, options);
+  }, [mapRef]);
 
-    return (
-        <MapContainer
-            ref={mapRef}
-            style={{
-                width: '500px',
-                height: '500px',
-            }}></MapContainer>
-    );
+  return <MapContainer ref={mapRef}></MapContainer>;
 };
 
 export default Map;
