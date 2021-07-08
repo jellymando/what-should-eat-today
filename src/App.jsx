77c2d844d64e8f53572cc17e9dccae9d58e9b6ac
jellymando <cicikdi@naver.com>
2021-07-08 21:43:26 +0900

    feat: addPlace 중복 에러 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PlaceList from "./pages/PlaceList";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/placeList">
-          <PlaceList />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/placeList">
+                    <PlaceList />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/PlaceList.jsx
+++ src/pages/PlaceList.jsx
@@ -5,32 +5,43 @@ import SearchBox from "../components/SearchBox";
 import ButtonBox from "../components/ButtonBox";
 
 const PlaceList = () => {
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [selectedPlace, setSelectedPlace] = useState({ name: "", x: 0, y: 0 });
-  const addPlace = async () => {
-    await axios.post("/places", {
-      name: selectedPlace.name,
-      latlng: {
-        x: selectedPlace.x,
-        y: selectedPlace.y,
-      },
-    });
-  };
+    const [searchKeyword, setSearchKeyword] = useState("");
+    const [selectedPlace, setSelectedPlace] = useState({ name: "", x: 0, y: 0 });
 
-  useEffect(() => {
-    console.log("selectedPlace", selectedPlace);
-  }, [selectedPlace]);
+    const addPlace = async () => {
+        try {
+            const {
+                data: { success, err },
+            } = await axios.post("/places", {
+                name: selectedPlace.name,
+                latlng: {
+                    x: selectedPlace.x,
+                    y: selectedPlace.y,
+                },
+            });
+            if (!success) {
+                switch (err.code) {
+                    case 11000:
+                        alert("이미 등록된 음식점입니다.");
+                        break;
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-  return (
-    <>
-      <SearchBox setSearchKeyword={setSearchKeyword} />
-      <PlaceListMap
-        searchKeyword={searchKeyword}
-        setSelectedPlace={setSelectedPlace}
-      />
-      <ButtonBox handleClick={addPlace} />
-    </>
-  );
+    useEffect(() => {
+        console.log("selectedPlace", selectedPlace);
+    }, [selectedPlace]);
+
+    return (
+        <>
+            <SearchBox setSearchKeyword={setSearchKeyword} />
+            <PlaceListMap searchKeyword={searchKeyword} setSelectedPlace={setSelectedPlace} />
+            <ButtonBox handleClick={addPlace} />
+        </>
+    );
 };
 
 export default PlaceList;
