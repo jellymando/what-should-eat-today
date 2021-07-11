67b3678476baf8ba0c8714e9073880ca352753cc
jellymando <cicikdi@naver.com>
2021-07-11 22:35:26 +0900

    feat: ButtonBox - handleButtonClick 분리

--- src/components/ButtonBox/index.jsx
+++ src/components/ButtonBox/index.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import { selectedPlaceState } from "store/atom";
-import { addPlace } from "api/place";
 import { ButtonWrap, Button } from "./styled";
 
-const ButtonBox = () => {
-    const selectedPlace = useRecoilValue(selectedPlaceState);
-
-    const addPlaceButtonHandler = async () => {
-        const { success, err } = await addPlace(selectedPlace);
-        if (!success) {
-            switch (err.code) {
-                case 11000:
-                    alert("이미 등록된 음식점입니다.");
-                    break;
-            }
-        }
-    };
-
+const ButtonBox = ({ handleButtonClick }) => {
     return (
         <ButtonWrap>
-            <Button onClick={() => addPlaceButtonHandler()}>밥집 추가</Button>
+            <Button onClick={() => handleButtonClick()}>밥집 추가</Button>
         </ButtonWrap>
     );
 };
--- src/pages/Place/index.jsx
+++ src/pages/Place/index.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
-import PlaceList from "../../components/PlaceList";
-import PlaceListMap from "../../components/PlaceListMap";
-import SearchBox from "../../components/SearchBox";
-import ButtonBox from "../../components/ButtonBox";
+import { useRecoilValue } from "recoil";
+import { selectedPlaceState } from "store/atom";
+import { addPlace } from "api/place";
+import PlaceList from "components/PlaceList";
+import PlaceListMap from "components/PlaceListMap";
+import SearchBox from "components/SearchBox";
+import ButtonBox from "components/ButtonBox";
 import { SideButton } from "./styled";
 
 const Place = () => {
     const [isAddPlace, setIsAddPlace] = useState(false);
+    const selectedPlace = useRecoilValue(selectedPlaceState);
+
+    const addPlaceButtonHandler = async () => {
+        const { success, err } = await addPlace(selectedPlace);
+        if (!success) {
+            switch (err.code) {
+                case 11000:
+                    alert("이미 등록된 음식점입니다.");
+                    break;
+            }
+        }
+    };
+
     return (
         <>
             {isAddPlace ? (
                 <>
                     <SearchBox />
                     <PlaceListMap />
-                    <ButtonBox />
+                    <ButtonBox handleButtonClick={addPlaceButtonHandler} />
                 </>
             ) : (
                 <PlaceList />
