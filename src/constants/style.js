ff27b9458417ae9c22ffe7ae456f4bd3fb28e1e6
jellymando <cicikdi@naver.com>
2021-07-11 23:57:02 +0900

    feat: color 상수 추가

--- src/constants/style.js
+++ src/constants/style.js
@@ -2,10 +2,11 @@ export const COLOR = {
     WHITE: "#fff",
     BLACK1: "#333",
     BLACK2: "#666",
-    ORANGE1: "#f15a22",
-    ORANGE2: "#f2784b",
     GRAY1: "#999",
     GRAY2: "#e9e9e9",
+    ORANGE1: "#f15a22",
+    ORANGE2: "#f2784b",
+    YELLOW1: "#fff9de",
 };
 
 export const FONTSIZE = {
