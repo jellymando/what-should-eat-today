da378db33862d6a82e3da29973ff28c9dfded91a
jellymando <cicikdi@naver.com>
2021-07-12 01:33:52 +0900

    feat: /keywords proxy middleware 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/places", {
-      target: "http://localhost:3001/",
-      changeOrigin: true,
-    })
-  );
+    app.use(
+        createProxyMiddleware("/places", {
+            target: "http://localhost:3001/",
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        createProxyMiddleware("/keywords", {
+            target: "http://localhost:3001/",
+            changeOrigin: true,
+        })
+    );
 };
