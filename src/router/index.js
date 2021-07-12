1fb128bcc41ca1dc4693e3c576e307402f6608dd
jellymando <cicikdi@naver.com>
2021-07-12 18:41:40 +0900

    feat: api - delete/keyword 추가, 에러 내려주는 값 수정

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 
     app.get("/places", (req, res) => {
         Place.find((err, data) => {
-            if (err) return res.status(500).send({ error: "database failure" });
+            if (err) return res.json({ success: false, err });
             res.json(data);
         });
     });
@@ -21,14 +21,14 @@ module.exports = function (app) {
 
     app.delete("/places/:id", (req, res) => {
         Place.deleteOne({ id: req.params.id }, (err, data) => {
-            if (err) return res.status(500).send({ error: "database failure" });
+            if (err) return res.json({ success: false, err });
             res.json(data);
         });
     });
 
     app.get("/keywords", (req, res) => {
         Keyword.find((err, data) => {
-            if (err) return res.status(500).send({ error: "database failure" });
+            if (err) return res.json({ success: false, err });
             res.json(data);
         });
     });
@@ -36,6 +36,13 @@ module.exports = function (app) {
     app.post("/keywords", (req, res) => {
         const keyword = new Keyword(req.body);
         keyword.save((err, doc) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).json({ success: true });
+        });
+    });
+
+    app.delete("/keywords/:_id", (req, res) => {
+        Keyword.deleteOne({ _id: req.params._id }, (err, data) => {
             if (err) return res.json({ success: false, err });
             return res.status(200).json({ success: true });
         });
