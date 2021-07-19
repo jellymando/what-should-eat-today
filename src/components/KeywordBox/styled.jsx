77cc88ea6a09d6feb5f9be885985926790962e31
jellymando <cicikdi@naver.com>
2021-07-19 13:06:27 +0900

    feat: KeywordBox styled 분리

--- src/components/KeywordBox/styled.jsx
+++ src/components/KeywordBox/styled.jsx
@@ -4,12 +4,8 @@ import { COLOR } from "constants/style";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    position: absolute;
     width: 100%;
-    left: 0;
-    bottom: 0;
-    padding: 8px 8px 0;
-    background: rgba(255, 255, 255, 0.8);
+    background: ${COLOR.WHITE};
     z-index: 99;
 `;
 
--- src/pages/Place/styled.jsx
+++ src/pages/Place/styled.jsx
@@ -1,25 +1,17 @@
 import styled from "styled-components";
-import { COLOR } from "constants/style";
+import { Container as KewordContainer } from "components/KeywordBox/styled";
 
 export const Container = styled.div`
     position: relative;
     display: flex;
     height: calc(100% - 110px);
     flex-direction: column;
-`;
 
-export const SideButton = styled.div`
-    position: fixed;
-    bottom: 65px;
-    right: 15px;
-    width: 50px;
-    height: 50px;
-    border-radius: 25px;
-    background: ${COLOR.ORANGE1};
-    background-size: 20px 20px;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-image: ${({ isAddPlace }) => (isAddPlace ? `url("/img/list-icon.png");` : `url("/img/add-icon.png");`)};
-    z-index: 9;
-    cursor: pointer;
+    ${KewordContainer} {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        padding: 8px 8px 0;
+        background: rgba(255, 255, 255, 0.8);
+    }
 `;
