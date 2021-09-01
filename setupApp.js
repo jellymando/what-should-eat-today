8fc615c7be19a5885b8fe90b83caadc7f7c16c79
jellymando <cicikdi@naver.com>
2021-09-01 22:40:55 +0900

    fix: setupApp 수정

--- setupApp.js
+++ setupApp.js
@@ -1,5 +1,3 @@
 const exec = require("child_process").exec;
-const dotenv = require("dotenv");
-dotenv.config({ path: ".env.domain" });
 
-exec(`HOST=${process.env.PROXY_DOMAIN} react-scripts start`);
+exec(`react-scripts start`);
