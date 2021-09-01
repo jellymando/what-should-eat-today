7ab2189aa0567e08f0b1e4962ee9c1fd3fde6e82
jellymando <cicikdi@naver.com>
2021-09-02 00:04:27 +0900

    fix: MongoDB 계정정보 보호

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -12,16 +13,16 @@ app.engine("html", require("ejs").renderFile);
 
 const Mongoose = require("mongoose");
 Mongoose.connect(
-  "mongodb+srv://jellymando:1016@cluster0.hcsaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
+    `mongodb+srv://${process.env.MONGODB_ACCOUNT}@cluster0.hcsaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }
 )
-  .then(() => console.log("mongoDB Connected..."))
-  .catch((e) => console.log(e));
+    .then(() => console.log("mongoDB Connected..."))
+    .catch((e) => console.log(e));
 
 app.use("/", express.static(__dirname + "/"));
 
